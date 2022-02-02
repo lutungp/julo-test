14eb6eebae9f5f1ea66e97891da3fe48323dad51
lintang permana <l>
2022-02-02 22:55:50 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func helloWorld(c *fiber.Ctx) error {
+	return c.SendString("Hello world")
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", helloWorld)
+
+	app.Listen(":3000")
+}
