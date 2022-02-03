c1b3ca4a9cd4fbe152bc433fb89694788b95fc53
lintang permana <l>
2022-02-03 14:00:56 +0700

    update

--- internal/routes/note/noteRoutes.go
+++ internal/routes/note/noteRoutes.go
@@ -2,15 +2,21 @@ package noteRoutes
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
 	noteHandler "github.com/lutungp/julo-test/internal/handler/note"
+	"github.com/lutungp/julo-test/middleware"
 )
 
 func SetupNoteRoutes(router fiber.Router) {
-	note := router.Group("/note")
+	note := router.Group("/note", middleware.JWTProtected())
 	// Create a Note
 	note.Post("/", noteHandler.CreateNotes)
 	// Read all Notes
-	note.Get("/", noteHandler.GetNotes)
+	note.Get("/", func(c *fiber.Ctx) error {
+		user := c.Locals("user").(*jwt.Token)
+		claims := user.Claims.(jwt.MapClaims)
+
+	})
 	// Read one Note
 	note.Get("/:noteId", noteHandler.GetNote)
 	// Update one Note
